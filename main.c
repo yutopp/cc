ebd0e0760130a0d39b12d7bcea612a780cc12fb9
yutopp <yutopp@gmail.com>
2018-09-15 22:04:11 +0900

    Invoke parser

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "lexer.h"
+#include "parser.h"
+#include "vector.h"
 
 char* read_all(FILE* fp) {
     fseek(fp, 0, SEEK_END);
@@ -35,6 +38,7 @@ int main(int argc, char *argv[]) {
 
     Lexer* lex = lexer_new(fcontent, fpath);
 
+    Vector* tokens = vector_new(sizeof(Token));
     for(;;) {
         Token tok = lexer_read(lex);
         token_fprint(stdout, &tok);
@@ -42,9 +46,35 @@ int main(int argc, char *argv[]) {
         if (tok.kind == TOK_KIND_EOF) {
             break;
         }
+
+        Token* elem = (Token*)vector_append(tokens);
+        assert(elem);
+        *elem = tok;
     }
 
     lexer_delete(lex);
+
+    Arena* nodes = arena_new();
+    Parser* parser = parser_new(tokens, nodes);
+
+    ParserResult res = parser_parse(parser);
+    if (res.result == PARSER_ERROR) {
+        fprintf(stderr, "ERROR: ");
+        parser_fprint_error(stderr, &res.error);
+        fprintf(stderr, "\n");
+
+        goto exit;
+    }
+    assert(res.value.node);
+
+    printf("\n");
+    printf("= AST =\n");
+    node_fprint(stdout, res.value.node);
+
+exit:
+    parser_drop(parser);
+    arena_drop(nodes);
+    vector_drop(tokens);
 
     free(fcontent);
 
