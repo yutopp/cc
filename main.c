014d79174259cee925d309e4c2ec6260877d8e6d
yutopp <yutopp@gmail.com>
2018-09-18 02:34:13 +0900

    Use all modules at main

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <assert.h>
 #include "lexer.h"
 #include "parser.h"
+#include "ir.h"
+#include "analyzer.h"
+#include "asm_x86_64.h"
 #include "vector.h"
 
 char* read_all(FILE* fp) {
@@ -18,9 +21,11 @@ char* read_all(FILE* fp) {
 }
 
 int main(int argc, char *argv[]) {
+    int exit_code = 1; // Default error
+
     if (argc != 2) {
         fprintf(stderr, "argc: %d shoule be 2", argc);
-        return 1;
+        goto exit_phase0;
     }
 
     char *fpath = argv[1];
@@ -28,7 +33,8 @@ int main(int argc, char *argv[]) {
     printf("%s\n", fpath);
     FILE *fp = fopen(fpath, "rb");
     if (fp == NULL) {
-        return 1;
+        fprintf(stderr, "Failed to open file: ");
+        goto exit_phase0;
     }
 
     char *fcontent = read_all(fp);
@@ -38,23 +44,24 @@ int main(int argc, char *argv[]) {
 
     Lexer* lex = lexer_new(fcontent, fpath);
 
-    Vector* tokens = vector_new(sizeof(Token));
+    Vector* tokens = vector_new(sizeof(Token)); // Vector<Token>
     for(;;) {
         Token tok = lexer_read(lex);
         token_fprint(stdout, &tok);
         printf("\n");
-        if (tok.kind == TOK_KIND_EOF) {
-            break;
-        }
 
         Token* elem = (Token*)vector_append(tokens);
         assert(elem);
         *elem = tok;
+
+        if (tok.kind == TOK_KIND_EOF) {
+            break;
+        }
     }
 
     lexer_delete(lex);
 
-    Arena* nodes = arena_new();
+    NodeArena* nodes = node_arena_new();
     Parser* parser = parser_new(tokens, nodes);
 
     ParserResult res = parser_parse(parser);
@@ -63,7 +70,7 @@ int main(int argc, char *argv[]) {
         parser_fprint_error(stderr, &res.error);
         fprintf(stderr, "\n");
 
-        goto exit;
+        goto exit_phase1;
     }
     assert(res.value.node);
 
@@ -71,12 +78,73 @@ int main(int argc, char *argv[]) {
     printf("= AST =\n");
     node_fprint(stdout, res.value.node);
 
-exit:
+    Analyzer* analyzer = analyzer_new();
+    analyzer_analyze(analyzer, res.value.node);
+    if (!analyzer_success(analyzer)) {
+        fprintf(stderr, "ERROR: analyzer");
+
+        goto exit_phase2;
+    }
+
+    IRBuilder* ir_builder = ir_builder_new();
+
+    IRModule* ir_mod = ir_builder_new_module(ir_builder, res.value.node);
+
+    printf("\n");
+    printf("= IR =\n");
+    ir_module_fprint(stdout, ir_mod);
+
+    ASM_X86_64* asm_x86_64 = asm_x86_64_new(ir_mod);
+
+    printf("\n");
+    printf("= ASM =\n");
+    asm_x86_64_fprint(stdout, asm_x86_64);
+    fflush(stdout);
+
+    // TODO: fix
+    FILE *fp_asm = fopen("_cc_out.s", "wb");
+    if (fp_asm == NULL) {
+        fprintf(stderr, "Failed to open asm file: ");
+
+        goto exit_phaseN;
+    }
+    asm_x86_64_fprint(fp_asm, asm_x86_64);
+    fflush(fp_asm);
+    fclose(fp_asm);
+
+    int cmd_res = system("as _cc_out.s -o _cc_out.o");
+    if (cmd_res != 0) {
+        fprintf(stderr, "FAILED: as");
+
+        goto exit_phaseN;
+    }
+
+    cmd_res = system("gcc _cc_out.o -o _cc_out.out");
+    if (cmd_res != 0) {
+        fprintf(stderr, "FAILED: gcc");
+
+        goto exit_phaseN;
+    }
+
+    exit_code = 0; // Success
+
+exit_phaseN:
+    asm_x86_64_drop(asm_x86_64);
+
+    ir_module_drop(ir_mod);
+    ir_builder_drop(ir_builder);
+
+exit_phase2:
+    analyzer_drop(analyzer);
+
+exit_phase1:
     parser_drop(parser);
-    arena_drop(nodes);
+    node_arena_drop(nodes);
+
     vector_drop(tokens);
 
     free(fcontent);
 
-    return 0;
+exit_phase0:
+    return exit_code;
 }
